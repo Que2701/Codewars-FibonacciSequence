3211565b03483a64b5789b916eadd266a20bb2d7
tumelo <tumelom@evolvemed.co.za>
2019-10-08 15:48:58 +0200

    fibonacci sequence

---
+++ Fibonacci/FibonacciApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FibonacciApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(fib(10));
+        }
+
+        public static int fib(int n)
+        {
+            int previousNumber = 0, currentNumber = 1, nextNumber = 0;
+            for (int i = 1; i < n; i++)
+            {
+                nextNumber = previousNumber + currentNumber;
+                previousNumber = currentNumber;
+                currentNumber = nextNumber;
+            }
+            // your code here!
+            return nextNumber;
+        }
+    }
+}
